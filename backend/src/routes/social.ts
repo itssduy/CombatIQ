ba42a752be6c7e89c3b2a2fecf37e8bf7c552b34
Duy <duypersonal512@gmail.com>
2026-03-02 13:46:39 -0600

    feat: Get social by athleteId endpoint

--- backend/src/routes/social.ts
+++ backend/src/routes/social.ts
@@ -16,11 +16,30 @@ socialRouter.get(":socialId", (req: Request, res: Response) => {
             }
         })
 
-        res.status(200).json({social: social})
+        res.status(200).json({social:social})
         
     } catch (err) {
         res.status(400).json({message: "unknown error"})
     }
 })
 
+socialRouter.get(":athleteId", (req: Request, res: Response) => {
+    try {
+        const {athleteId} = req.params
+        if (typeof athleteId !== "string") { 
+            return res.status(400).json({message: "unknown error"})
+        } 
+        
+        let social = prisma.social.findMany({
+            where: {
+                athleteId: athleteId
+            }
+        })
+
+        res.status(200).json({social: social})
+        
+    } catch (err) {
+        res.status(400).json({message: "unknown error"})
+    }
+})
 export default socialRouter
