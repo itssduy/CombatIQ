ab984d619101c5e6bb12010709bcc992df2898ff
Duy <duypersonal512@gmail.com>
2026-03-02 00:50:20 -0600

    feat: Create get athlete endpoint

--- backend/src/routes/athlete.ts
+++ backend/src/routes/athlete.ts
@@ -11,3 +11,21 @@ athleteRouter.get("/", async (req: Request, res: Response) => {
         res.status(400).json({message: "error"})
     }
 })
+athleteRouter.get(":athleteId", async (req: Request, res: Response) => {
+    try {
+        const athleteId = req.params.athleteId
+
+        if (typeof athleteId !== "string") {
+            return res.status(400).json({ message: "Invalid userId" });
+        }
+
+        let athletes = await prisma.athlete.findUnique({
+            where: {
+                id: athleteId
+            }
+        })
+        res.json({athletes: athletes})
+    } catch (err) {
+        res.status(400).json({message: "error"})
+    }
+})
